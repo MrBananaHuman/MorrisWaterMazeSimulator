a91fd966a8e7304b9a1a3f96b7395c6711e44106
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2020-08-06 13:00:29 +0900

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def get_state():
     state = conn.recv(1024).decode()
     if not state:
         return null
-    return str(state)	# trial_number / reward / d_from_east / d_from_west / d_from_south / d_from_north
+    return str(state)	#  reward / d_from_east / d_from_west / d_from_south / d_from_north
 
 def send_action(action_number):
     conn.send(str(action_number).encode())
